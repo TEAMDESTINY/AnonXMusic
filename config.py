834e7107dcfbdf431d08dab33784808dfb005d6d
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-01-13 18:30:50 +0530

    Update config.py
    Signed-off-by: ⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/codebotnetwork")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Yours_Soull")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
@@ -32,9 +32,9 @@ class Config:
             url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
         ]
-        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://graph.org/file/329219144d5e7afa8a736-31419f4b7ff8e8724d.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://graph.org/file/329219144d5e7afa8a736-31419f4b7ff8e8724d.jpg")
+        self.START_IMG = getenv("START_IMG", "https://graph.org/file/329219144d5e7afa8a736-31419f4b7ff8e8724d.jpg")
 
     def check(self):
         missing = [
